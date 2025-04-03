b87b7406dbfd2832644b912a24dd916f322fd11e
Joacohazee <joacohaze@gmail.com>
2025-04-03 12:03:46 -0300

    fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: ["./tsconfig.json"],
-      tsconfigRootDir: __dirname,
-      sourceType: "module",
-      ecmaVersion: "latest",
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  extends: [
+    "airbnb-typescript/base",
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".next/"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    plugins: ["react", "@typescript-eslint", "prettier"],
-    extends: [
-      "airbnb-typescript/base",
-      "next/core-web-vitals",
-      "plugin:prettier/recommended",
-    ],
-    rules: {
-      quotes: ["error", "double"],
-    },
-    ignorePatterns: ["node_modules/", "dist/", ".next/"],
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  };
-  
+  },
+};
